ae4b871bdfb730553b947c9ec09a9d8853bf7fa5
Владислав Юров <clydeurov@gmail.com>
2022-10-17 10:37:10 +0300

    Уточник названия функций

--- show_salary_table.py
+++ show_salary_table.py
@@ -4,7 +4,7 @@ import get_vacancies_dict_for_hh
 import get_vacancies_dict_for_sj
 
 
-def get_list(vacancies, line=1):
+def get_salaries(vacancies, line=1):
     head_line = [
         "Язык программирование",
         "Вакансий найдено",
@@ -34,12 +34,12 @@ def main():
     ]
 
     title_for_hh = "HeadHunter Moscow"
-    vacancies_hh = get_vacancies_dict_for_hh.get_vacancies_hh(languages)
-    table_instance_hh = AsciiTable(get_list(vacancies_hh), title_for_hh)
+    vacancies_hh = get_vacancies_dict_for_hh.main(languages)
+    table_instance_hh = AsciiTable(get_salaries(vacancies_hh), title_for_hh)
 
     title_for_sj = "SuperJob Moscow"
     vacancies_sj = get_vacancies_dict_for_sj.main(languages)
-    table_instance_sj = AsciiTable(get_list(vacancies_sj), title_for_sj)
+    table_instance_sj = AsciiTable(get_salaries(vacancies_sj), title_for_sj)
     print(table_instance_hh.table, "\n", table_instance_sj.table)
 
 
